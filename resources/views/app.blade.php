d2afb9d89f15a4a0032245e397399870578a5fcb
Grant Bennett <subs@woode.co.uk>
2026-08-06 00:08:59 +0100

    rebrand from the starter kit to Dev Stats

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,15 +30,13 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
 
         @vite(['resources/css/app.css', 'resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'Dev Stats') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
